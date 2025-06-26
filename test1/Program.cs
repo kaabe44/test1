e049f98bfa51edeba99e48815e12a20745dacb93
kaabe <kaabacadde18@gmail.com>
2025-06-26 10:18:24 +0300

    badalida koobaad

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare variables
+        string name = "Abdulahi mohamed husein";
+        string motherName = "maryan maxamed";
+        DateTime dob = new DateTime(2003, 5, 12); // Format: YYYY, MM, DD
+        string pob = "bookh";
+        string email = "kaabacadde18@gmail.com";
+        char sex = 'M'; // 'M' for Male, 'F' for Female
+        string telephone = "+252907865222";
+        string status = "Single"; // Can be "Single", "Married", etc.
+
+        // Output the variables
+        Console.WriteLine("Personal Information:");
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Mother's Name: {motherName}");
+        Console.WriteLine($"Date of Birth: {dob.ToShortDateString()}");
+        Console.WriteLine($"Place of Birth: {pob}");
+        Console.WriteLine($"Email: {email}");
+        Console.WriteLine($"Sex: {sex}");
+        Console.WriteLine($"Telephone: {telephone}");
+        Console.WriteLine($"Status: {status}");
+    }
+}
